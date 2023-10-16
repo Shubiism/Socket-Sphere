f4af916e4d410ab9b9d6fd648e972d1d32e0541e
Shubiism <dash.subham.dash@gmail.com>
2023-10-16 22:17:50 +0530

    Some changes made.

--- js/client.js
+++ js/client.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:8000');
+const socket = io('https://socket-sphere-backend-production.up.railway.app');
 
 const form = document.getElementById('send-container');
 const msgInp = document.getElementById('msgInp');
